f1eb18d1d640bd7a1c794f5bc500d8b7aacd4a69
Ahmed Nafisul Bari <nafisulbari@gmail.com>
2020-03-18 07:35:15 +0600

    Housekeeping

--- src/main/java/com/nafisulbari/weather/Main.java
+++ src/main/java/com/nafisulbari/weather/Main.java
@@ -1,7 +1,6 @@
 package com.nafisulbari.weather;
 
 
-
 import com.nafisulbari.weather.service.WeatherService;
 
 
@@ -10,18 +9,16 @@ import java.io.IOException;
 /**
  * A simple weather app
  *
- * @author  Ahmed Nafisul Bari
+ * @author Ahmed Nafisul Bari
  */
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-WeatherService weatherService=new WeatherService();
+        WeatherService weatherService = new WeatherService();
 
         System.out.println(weatherService.getWeatherData().get("currently").getAsJsonObject().get("summary"));
-
-        System.out.println(weatherService.getLatLong());
-
+        
         System.out.println("Array");
         System.out.println(weatherService.getWeatherData().get("hourly").getAsJsonObject().get("data").getAsJsonArray());
 
--- src/main/java/com/nafisulbari/weather/service/WeatherService.java
+++ src/main/java/com/nafisulbari/weather/service/WeatherService.java
@@ -7,18 +7,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class WeatherService {
 
 
-    public JsonObject getWeatherData(){
+    /**
+     * //todo
+     *
+     * @return weather data as json Object
+     */
 
-        JsonObject jobj= new JsonObject();
+    public JsonObject getWeatherData() {
+
+        JsonObject jObj = new JsonObject();
+
         try {
 
-            URL url = new URL("https://api.darksky.net/forecast/d257f7195d5a5ac353a59304d20032db/"+getLatLong());
+            URL url = new URL("https://api.darksky.net/forecast/d257f7195d5a5ac353a59304d20032db/" + getLatLong());
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept", "application/json");
@@ -31,41 +37,41 @@ public class WeatherService {
             BufferedReader br = new BufferedReader(new InputStreamReader(
                     (conn.getInputStream())));
 
+            String json = br.readLine();
 
-            System.out.println("Output from Server .... \n");
-            String output = br.readLine();
-            System.out.println(output);
+            System.out.println("Weather Data .... \n");
+            System.out.println(json);
 
-            String json = output;
-            jobj = new Gson().fromJson(json, JsonObject.class);
-
+            jObj = new Gson().fromJson(json, JsonObject.class);
             conn.disconnect();
 
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-
         }
 
-        return jobj;
+        return jObj;
     }
 
-    public String getLatLong(){
 
-        JsonObject jobj= new JsonObject();
+    /**
+     * gets public ip address of machine,
+     * fetches location value from ip address.
+     *
+     * @return machines latitude,longitude.
+     */
+    public String getLatLong() {
+
+        JsonObject jObj = new JsonObject();
+
         try {
 
             URL url_name = new URL("http://bot.whatismyipaddress.com");
-
             BufferedReader sc =
                     new BufferedReader(new InputStreamReader(url_name.openStream()));
-
-            // reads system IPAddress
-            String systemipaddress = sc.readLine().trim();
+            String machinesIpAddress = sc.readLine().trim();
 
 
-            URL url = new URL("http://ip-api.com/json/"+systemipaddress);
+            URL url = new URL("http://ip-api.com/json/" + machinesIpAddress);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept", "application/json");
@@ -78,27 +84,21 @@ public class WeatherService {
             BufferedReader br = new BufferedReader(new InputStreamReader(
                     (conn.getInputStream())));
 
-            String output;
-            System.out.println("Output from Server .... \n");
-            output = br.readLine();
-            System.out.println(output);
 
-            String json = output;
-            jobj = new Gson().fromJson(json, JsonObject.class);
+            String json = br.readLine();
+
+            System.out.println("Location data .... \n");
+            System.out.println(json);
 
 
+            jObj = new Gson().fromJson(json, JsonObject.class);
             conn.disconnect();
 
-        } catch (MalformedURLException e) {
-
-            e.printStackTrace();
-
         } catch (IOException e) {
-
             e.printStackTrace();
 
         }
-        System.out.println("LOC: "+jobj.get("lat")+","+jobj.get("lon"));
-        return jobj.get("lat")+","+jobj.get("lon");
+        System.out.println("LOC: " + jObj.get("lat") + "," + jObj.get("lon"));
+        return jObj.get("lat") + "," + jObj.get("lon");
     }
 }
