6d8a88d6664af336c3263172e1e7b2f77ddfe2c3
Ahmed Nafisul Bari <nafisulbari@gmail.com>
2020-03-19 04:03:47 +0600

    Added get and set methods and degree label's moveclick event

--- src/main/java/com/nafisulbari/weather/controller/Controller.java
+++ src/main/java/com/nafisulbari/weather/controller/Controller.java
@@ -1,4 +1,66 @@
 package com.nafisulbari.weather.controller;
 
-public class Controller {
+import com.nafisulbari.weather.service.WeatherService;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+
+    WeatherService weatherService = new WeatherService();
+
+    @FXML
+    GridPane gridPane;
+    @FXML
+    Label degree;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+
+    //-----------------------Get & Set methods for Controller injection--------------------------------------------
+    //-------------------------------------------------------------------------------------------------------------
+
+    public void setDegree(String degreeVal) {
+        this.degree.setText(degreeVal);
+    }
+
+
+
+
+
+
+    //-------------------------End of Get & Set methods-------------------------------------------------------------
+
+
+
+
+
+
+
+    //-------------------------Controller Methods-------------------------------------------------------------------
+    //--------------------------------------------------------------------------------------------------------------
+
+    public void degreeOnClicked(MouseEvent mouseEvent) {
+        degree.setText(weatherService.getWeatherData().get("currently").getAsJsonObject().get("temperature").toString());
+    }
+
+
+
+
+
+
+    //------------------------End of Controller Methods--------------------------------------------------------------
+
 }
