a7f5288ff725aeb8183482bcd33b0408c797ca14
Ahmed Nafisul Bari <nafisulbari@gmail.com>
2020-03-18 06:33:35 +0600

    Basic weather data fetching  from ip address

--- src/main/java/com/nafisulbari/weather/Main.java
+++ src/main/java/com/nafisulbari/weather/Main.java
@@ -1,7 +1,30 @@
 package com.nafisulbari.weather;
 
+
+
+import com.nafisulbari.weather.service.WeatherService;
+
+
+import java.io.IOException;
+
+/**
+ * A simple weather app
+ *
+ * @author  Ahmed Nafisul Bari
+ */
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+WeatherService weatherService=new WeatherService();
+
+        System.out.println(weatherService.getWeatherData().get("currently").getAsJsonObject().get("summary"));
+
+        System.out.println(weatherService.getLatLong());
+
+        System.out.println("Array");
+        System.out.println(weatherService.getWeatherData().get("hourly").getAsJsonObject().get("data").getAsJsonArray());
+
 
     }
 }
--- src/main/java/com/nafisulbari/weather/service/WeatherService.java
+++ src/main/java/com/nafisulbari/weather/service/WeatherService.java
@@ -1,4 +1,104 @@
 package com.nafisulbari.weather.service;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class WeatherService {
+
+
+    public JsonObject getWeatherData(){
+
+        JsonObject jobj= new JsonObject();
+        try {
+
+            URL url = new URL("https://api.darksky.net/forecast/d257f7195d5a5ac353a59304d20032db/"+getLatLong());
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+
+            System.out.println("Output from Server .... \n");
+            String output = br.readLine();
+            System.out.println(output);
+
+            String json = output;
+            jobj = new Gson().fromJson(json, JsonObject.class);
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+        return jobj;
+    }
+
+    public String getLatLong(){
+
+        JsonObject jobj= new JsonObject();
+        try {
+
+            URL url_name = new URL("http://bot.whatismyipaddress.com");
+
+            BufferedReader sc =
+                    new BufferedReader(new InputStreamReader(url_name.openStream()));
+
+            // reads system IPAddress
+            String systemipaddress = sc.readLine().trim();
+
+
+            URL url = new URL("http://ip-api.com/json/"+systemipaddress);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            output = br.readLine();
+            System.out.println(output);
+
+            String json = output;
+            jobj = new Gson().fromJson(json, JsonObject.class);
+
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+        System.out.println("LOC: "+jobj.get("lat")+","+jobj.get("lon"));
+        return jobj.get("lat")+","+jobj.get("lon");
+    }
 }
