687914e04aa133adadfe9cd412ff33b72e00b7f6
Ahmed Nafisul Bari <nafisulbari@gmail.com>
2020-03-19 03:58:30 +0600

    Start method loads the controller,sets value to label and passes it to the app

--- src/main/java/com/nafisulbari/weather/Main.java
+++ src/main/java/com/nafisulbari/weather/Main.java
@@ -1,11 +1,12 @@
 package com.nafisulbari.weather;
 
 
+import com.nafisulbari.weather.controller.Controller;
 import com.nafisulbari.weather.service.WeatherService;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 
@@ -22,12 +23,13 @@ public class Main extends Application {
 
         launch(args);
 
-        WeatherService weatherService = new WeatherService();
-
-        System.out.println(weatherService.getWeatherData().get("currently").getAsJsonObject().get("summary"));
-        
-        System.out.println("Array");
-        System.out.println(weatherService.getWeatherData().get("hourly").getAsJsonObject().get("data").getAsJsonArray());
+//        WeatherService weatherService = new WeatherService();
+//
+//        System.out.println(weatherService.getWeatherData().get("currently").getAsJsonObject().get("temperature").toString());
+//
+//        System.err.println(weatherService.getWeatherData().get("currently").getAsJsonObject().get("temperature").toString());
+//
+//        System.out.println(weatherService.getWeatherData().get("hourly").getAsJsonObject().get("data").getAsJsonArray());
 
 
     }
@@ -36,9 +38,27 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+
+        WeatherService weatherService = new WeatherService();
+
+
+
+        GridPane gridPane =loader.load();
+
+        //getting controller from the fxml file.
+        Controller controller =loader.getController();
+
+
+        //setting degree value from the Controller.setDegree(String degreeVal) method.
+        controller.setDegree(weatherService.getWeatherData().get("currently").getAsJsonObject().get("temperature").toString());
+
+        Scene scene =  new Scene(gridPane,300,275);
+        primaryStage.setScene(scene);
+
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 }
