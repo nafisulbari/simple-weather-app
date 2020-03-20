da85251ae18577ec212f89150bc6d10e2633be6a
Ahmed Nafisul Bari <nafisulbari@gmail.com>
2020-03-20 06:06:46 +0600

    Added get set methods for daily weather update.

--- src/main/java/com/nafisulbari/weather/controller/Controller.java
+++ src/main/java/com/nafisulbari/weather/controller/Controller.java
@@ -78,12 +78,92 @@ public class Controller implements Initializable {
         realFeel.setText(realFeelVal);
     }
 
+    public void setLabel1(String label1) {
+        this.label1.setText(label1);
+    }
+
+    public void setLabel1H(String label1H) {
+        this.label1H.setText(label1H);
+    }
+
+    public void setLabel1L(String label1L) {
+        this.label1L.setText(label1L);
+    }
+
+    public void setLabel2(String label2) {
+        this.label2.setText(label2);
+    }
+
+    public void setLabel2H(String label2H) {
+        this.label2H.setText(label2H);
+    }
+
+    public void setLabel2L(String label2L) {
+        this.label2L.setText(label2L);
+    }
+
+    public void setLabel3(String label3) {
+        this.label3.setText(label3);
+    }
+
+    public void setLabel3H(String label3H) {
+        this.label3H.setText(label3H);
+    }
+
+    public void setLabel3L(String label3L) {
+        this.label3L.setText(label3L);
+    }
+
+    public void setLabel4(String label4) {
+        this.label4.setText(label4);
+    }
+
+    public void setLabel4H(String label4H) {
+        this.label4H.setText(label4H);
+    }
+
+    public void setLabel4L(String label4L) {
+        this.label4L.setText(label4L);
+    }
+
+    public void setLabel5(String label5) {
+        this.label5.setText(label5);
+    }
+
+    public void setLabel5H(String label5H) {
+        this.label5H.setText(label5H);
+    }
+
+    public void setLabel5L(String label5L) {
+        this.label5L.setText(label5L);
+    }
+
+    public void setLabel6(String label6) {
+        this.label6.setText(label6);
+    }
+
+    public void setLabel6H(String label6H) {
+        this.label6H.setText(label6H);
+    }
+
+    public void setLabel6L(String label6L) {
+        this.label6L.setText(label6L);
+    }
+
+    public void setLabel7(String label7) {
+        this.label7.setText(label7);
+    }
+
+    public void setLabel7H(String label7H) {
+        this.label7H.setText(label7H);
+    }
+
+    public void setLabel7L(String label7L) {
+        this.label7L.setText(label7L);
+    }
 
 
-
-
-
-    //-------------------------End of Get & Set methods-------------------------------------------------------------
+//-------------------------End of Get & Set methods-------------------------------------------------------------
 
 
 
